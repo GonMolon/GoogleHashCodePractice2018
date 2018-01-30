a4496a72e5f2759e82c22e32d91bbf6d572023fe
Gonzalo Solera <gonzalo1796@gmail.com>
2018-01-30 20:15:52 +0100

    Implementing read of pizza layout

--- TestProblem/src/TestProblem/State/PizzaLayout.java
+++ TestProblem/src/TestProblem/State/PizzaLayout.java
@@ -1,5 +1,6 @@
 package TestProblem.State;
 
+import java.util.ArrayList;
 import java.util.stream.Stream;
 
 public class PizzaLayout {
@@ -9,16 +10,53 @@ public class PizzaLayout {
     protected static int L;
     protected static int H;
 
-
+    public Cell[][] layout;
 
     public PizzaLayout(Stream<String> input) {
-        String[] dimensions = input.findFirst().get().split("\\s+");
+        ArrayList<String> lines = new ArrayList<>();
+        input.forEach(lines::add);
+
+        String[] dimensions = lines.remove(0).split("\\s+");
         R = Integer.parseInt(dimensions[0]);
         C = Integer.parseInt(dimensions[1]);
         L = Integer.parseInt(dimensions[2]);
         H = Integer.parseInt(dimensions[3]);
 
-
+        layout = new Cell[R][C];
+        for(int i = 0; i < R; ++i) {
+            for(int j = 0; j < C; ++j) {
+                char c = lines.get(i).charAt(j);
+                Ingredient ingredient = c == 'M' ? Ingredient.M : Ingredient.T;
+                layout[i][j] = new Cell(ingredient, false);
+            }
+        }
     }
 
+    public void to_string() {
+        for(int i = 0; i < R; ++i) {
+            for(int j = 0; j < C; ++j) {
+                Cell c = layout[i][j];
+                if(c.used) {
+                    System.out.print('.');
+                } else {
+                    System.out.print(c.ingredient);
+                }
+            }
+            System.out.print('\n');
+        }
+    }
+
+    public class Cell {
+        public Ingredient ingredient;
+        public boolean used;
+
+        public Cell(Ingredient ingredient, boolean used) {
+            this.ingredient = ingredient;
+            this.used = used;
+        }
+    }
+
+    public enum Ingredient {
+        T, M
+    }
 }
